d42428622e7acccfc0ec880c6bc42a6b7df566aa
Georgi Santana <georgi.andersson@hotmail.com>
2022-09-22 11:02:09 -0300

    Criado lógica para bottuns, number e operation

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,18 @@ const operacaoAnteriorText = document.querySelector("#operacao-anterior");
 const operacaoAtualText = document.querySelector("#operacao-atual");
 const buttons = document.querySelectorAll("#buttons-container button");
 
+class calculador {
+  constructor() {}
+}
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", (evento) => {
     const valor = evento.target.innerText;
-    console.log(valor);
+
+    if (parseInt(valor) >= 0 || parseInt(valor) === ".") {
+      console.log(valor);
+    } else {
+      console.log("Operacão " + valor);
+    }
   });
 });
